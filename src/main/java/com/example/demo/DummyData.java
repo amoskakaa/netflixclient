0cbb390b76b650778ca2fd78e8f857cc486b96de
Amos Kakaa <amoskakaa@gmail.com>
2019-10-31 10:24:00 +0300

    Initial commit

--- src/main/java/com/example/demo/DummyData.java
+++ src/main/java/com/example/demo/DummyData.java
@@ -27,19 +27,23 @@ public class DummyData implements CommandLineRunner {
 
         //Client to View all categories
         List<Category> availableCategory = feignRestClient.viewCategories();
+        System.out.println("...........Fetching Categories.............");
         System.out.println(availableCategory);
 //
         //Client to view specific category
         Category viewSpecific = feignRestClient.findByCategoryId(8L);
+        System.out.println("...........Fetching Category.............");
         System.out.println(viewSpecific);
 //
-//        //Client to View Movies
-//        List<Movies> availableMovies = feignRestClient.viewMovies();
-//        System.out.println(availableMovies);
+        //Client to View Movies
+        List<Movies> availableMovies = feignRestClient.viewMovies();
+        System.out.println("...........Fetching Movies.............");
+        System.out.println(availableMovies);
 //
 //        //Client to view specific movie
-//        Movies viewSpecificMovie = feignRestClient.findByMovieId(3L);
-//        System.out.println(viewSpecificMovie);
+        Movies viewSpecificMovie = feignRestClient.findByMovieId(3L);
+        System.out.println("...........Fetching Movie.............");
+        System.out.println(viewSpecificMovie);
 //
 ////        //Client suggest movie
 //        Movies newmovie = feignRestClient.createMovies(new Movies("XMen","Suggested",availableCategory));
